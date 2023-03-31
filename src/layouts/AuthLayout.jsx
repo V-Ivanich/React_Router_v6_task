47698b67233a1149b7b3053b65db1ae92fe733e8
V-Ivanich <chipai_dom@mail.ru>
2023-03-31 21:15:29 +0300

    outlet

---
+++ src/layouts/AuthLayout.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+// Librares
+import { useSelector } from 'react-redux'
+import { Outlet } from 'react-router-dom'
+
+// Components
+import Card from '../components/Card'
+
+// Store
+import { isLoggedInSelector } from '../store/authSlice'
+
+const AuthLayout = () => {
+  // let { path } = useRouteMatch()
+  const isLoggedIn = useSelector(isLoggedInSelector())
+  console.log(isLoggedIn)
+
+  // if (isLoggedIn) {
+  //   return <Redirect to='/' />
+  // }
+
+  return (
+    <div className='flex grow flex-col justify-center items-center  dark:text-slate-200 '>
+      <Card>
+        <Outlet />
+      </Card>
+    </div>
+  )
+}
+
+export default AuthLayout
